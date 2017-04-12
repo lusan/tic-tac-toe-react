4707dc45c72bd9b630c7bcf93d2cc560ab23a3df
Lusan <daslusan@lifcare.in>
2017-04-12 12:29:30 +0530

    handling click and converting to stateless function square

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react'
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
   render() {
     const status = 'Next player: X';
